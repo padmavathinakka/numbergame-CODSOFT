c10fbaf6b2fde16f3d3956d0f660f3e08f99ed2e
padmavathinakka <149475722+padmavathinakka@users.noreply.github.com>
2024-08-08 08:00:40 +0530

    Update numbergame2.java

--- numbergame2.java
+++ numbergame2.java
@@ -1,6 +1,6 @@
 import java.util.Random; 
 import java.util.Scanner;
-class NumberGame { 
+class Main { 
 public static void main(String args[]) { 
 Scanner scan = new Scanner(System.in);
 Random rand = new Random();
